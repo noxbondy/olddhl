1919c2f26d3fede13f7e2c0d3e9ed7a7ad566f6b
md gulam noxbondy <gulamnoxbondy@gmail.com>
2026-09-13 17:39:47 +0200

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
- import "bootstrap/dist/css/bootstrap.min.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import "bulma/css/bulma.min.css";
- import 'uikit/dist/css/uikit.min.css'
+import "uikit/dist/css/uikit.min.css";
 
+// PWA temporarily disabled
+// import { registerSW } from "virtual:pwa-register";
 
-import { registerSW } from 'virtual:pwa-register'
+// registerSW({
+//   immediate: true,
+//   onNeedRefresh() {
+//     window.location.reload();
+//   },
+// });
 
-// ✅ This registers the service worker
-registerSW({ immediate: true })
-console.log("Loaded API URL:", import.meta.env.VITE_API_URL);
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
