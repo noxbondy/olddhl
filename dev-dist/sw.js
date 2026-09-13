cbc7322b0e8c7087104ae48a1ee3d8a2e5b11fc1
md gulam noxbondy <gulamnoxbondy@gmail.com>
2026-09-13 22:02:11 +0200

    new update

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.lttgd8hlea8"
+    "revision": "0.hbursb1trf8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
