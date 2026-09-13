f66828f19161c7be6a5756f90c37681acd8409b5
md gulam noxbondy <gulamnoxbondy@gmail.com>
2026-09-13 13:12:02 +0200

    css

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import "bootstrap/dist/css/bootstrap.min.css";
+ import "bootstrap/dist/css/bootstrap.min.css";
 import "bulma/css/bulma.min.css";
-import 'uikit/dist/css/uikit.min.css'
+ import 'uikit/dist/css/uikit.min.css'
+
 
 import { registerSW } from 'virtual:pwa-register'
 
