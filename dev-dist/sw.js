eabb3f79b6320a5322ca69f5b650aa49eacb6144
md gulam noxbondy <gulamnoxbondy@gmail.com>
2026-09-13 17:55:14 +0200

    css

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.1ocdbq43t4"
+    "revision": "0.e116h8j4ng8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
