82dc7764739027b66cda2dfa3a29c3477f28b09c
md gulam noxbondy <gulamnoxbondy@gmail.com>
2026-09-13 21:14:13 +0200

    boo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
 
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
